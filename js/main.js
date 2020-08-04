438e712468a49596345a3eee7f1d3092854a0cbb
Paulo Soares <soaresprogramador@gmail.com>
2020-08-04 12:02:18 -0300

    análise básica de CEP

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert("funcionou");
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type:"GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro)
+
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("estado").innerHTML = response.uf;
+        }        
+    })
+}
